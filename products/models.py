ad7027543be6ea77541fe66a035ef20f0b32c74c
Tordai Janos <janos.tordai@zamg.ac.at>
2020-02-07 16:13:00 +0000

    fff

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class ProductRequest(models.Model):
+    product_type = models.CharField(max_length=40)
+    parameter = models.CharField(max_length=30)
+    parameter2 = models.CharField(max_length=30)
+    aggregation_period = models.CharField(max_length=10)
+    season = models.CharField(max_length=10)
+    scenario = models.CharField(max_length=10)
+    region_option = models.CharField(max_length=20)
+    region = models.CommaSeparatedIntegerField()
+    period_start = models.DateField()
+    period_end = models.DateField()
+    reference_period_start = models.DateField()
+    reference_period_end = models.DateField()
+    
