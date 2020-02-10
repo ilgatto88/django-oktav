808a36d62e12153bf68142c337697a057f29270f
Tordai Janos <janos.tordai@zamg.ac.at>
2020-02-10 11:34:32 +0000

    working

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from products.models import ProductRequest
 
 # Register your models here.
+admin.site.register(ProductRequest)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from .models import ProductRequest
 
 # Create your views here.
+def home(request):
+    products_requests = ProductRequest.objects.all()
+    return render(request, 'home.html', {'product_requests': products_requests})
